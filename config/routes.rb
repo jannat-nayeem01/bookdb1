060cbba84d1b6e5b4bc48cd42312884f1eca6bde
Jannatun Nayeem <jannat.nayeem01@gmail.com>
2023-10-05 14:21:28 +0700

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :readers
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   root "books#index"
   #get 'readers#index'
   #get 'books/show'
+  get 'users/new'
 end
